46343ac884a5bbb05375d4b35da5a1cca48f15dd
Yazan Sadieh <1942022@uni-wuppertal.de>
2021-12-01 22:34:38 +0100

    a

--- BM/src/application/GamePanel.java
+++ BM/src/application/GamePanel.java
@@ -1,36 +1,19 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javafx.scene.Group;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.Light.Point;
-import javafx.scene.image.Image;
 
 public class GamePanel {
-	private static final int WIDTH = 800;
-    private static final int HEIGHT = WIDTH;
-    private static final int ROWS = 20;
-    private static final int COLUMNS = ROWS;
-    private static final int SQUARE_SIZE = WIDTH / ROWS;
+	//private static final int WIDTH ;
+    //private static final int HEIGHT ;
+    //private static final int ROWS ;
+    //private static final int COLUMNS;
+    //private static final int SQUARE_SIZE ;
     
     
-    private GraphicsContext gc;
-    private List<Point> snakeBody = new ArrayList();
-    private Point snakeHead;
-    private Image foodImage;
-    private int foodX;
-    private int foodY;
-    private boolean gameOver;
-    private int currentDirection;
-    private int score = 0;
+
+
 	
 	 public GamePanel()  {
-		Group root = new Group();
-        Canvas canvas = new Canvas(WIDTH, HEIGHT);
-        root.getChildren().add(canvas);
+	
 	}
 	public void init() {};
 
