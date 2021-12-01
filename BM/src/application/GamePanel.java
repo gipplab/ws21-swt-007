6b9454ef46c84190a2f67b74d1bbea971b25f9a5
Yazan Sadieh <1942022@uni-wuppertal.de>
2021-12-01 22:33:06 +0100

    SinglePlayPanel Hinzugefügt

--- BM/src/application/GamePanel.java
+++ BM/src/application/GamePanel.java
@@ -1,9 +1,37 @@
 package application;
 
-public class GamePanel {
+import java.util.ArrayList;
+import java.util.List;
 
-	public GamePanel() {
-		// TODO Auto-generated constructor stub
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Light.Point;
+import javafx.scene.image.Image;
+
+public class GamePanel {
+	private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+    
+    
+    private GraphicsContext gc;
+    private List<Point> snakeBody = new ArrayList();
+    private Point snakeHead;
+    private Image foodImage;
+    private int foodX;
+    private int foodY;
+    private boolean gameOver;
+    private int currentDirection;
+    private int score = 0;
+	
+	 public GamePanel()  {
+		Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
 	}
+	public void init() {};
 
 }
--- BM/src/application/Main.java
+++ BM/src/application/Main.java
@@ -3,14 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import uet.oop.bomberman.GamePanel;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+	
+		
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Scenes/LandingPage.fxml"));
 			Scene scene = new Scene(root);
