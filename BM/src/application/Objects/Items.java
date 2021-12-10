78582ecbcec2d592d4304fb9aa6809bd0f94a0af
BergKabs <92931496+BergKabs@users.noreply.github.com>
2021-12-10 18:55:02 +0100

    Add files via upload
    Die Klassen die ich grad mache

---
+++ BM/src/application/Objects/Items.java
@@ -0,0 +1,43 @@
+package applic.objects;
+
+import bomberman.java;
+
+public class bombentasche {
+	int bombenanzahl=5;
+	
+	
+	//Erhöht die Maximal Anzahl der Bomben die ein Spieler tragen kann .
+	int increasebombs();
+	
+	
+}
+
+public class flamme{
+	float explosionincrease;
+	
+	
+	//Erhöht den Explosionsradius der Bomben die der Spieler legt.
+	float increaseexplosion() {
+		
+	}
+}
+
+public class laufschuhe {
+	float speedpoints;
+	
+	
+	// Erhöht die Bewegungsgeschwindigkeit des Spielers
+	float increasespeed()
+}
+
+public class herz {
+	int healthpoints;
+	
+	
+	//Erhöht die Gesundheitspunkte der Bombermancharakters
+    	int increasehealth(int bomberman.health ){
+		
+		
+	}
+	
+}
---
+++ BM/src/application/Objects/keyboard.java
@@ -0,0 +1,14 @@
+package applic.objects;
+
+public class keyboard {
+
+boolean keyup = false ;
+boolean keydown = false;
+boolean keyright = false;
+boolean keyleft = false ;
+
+
+
+
+
+}
