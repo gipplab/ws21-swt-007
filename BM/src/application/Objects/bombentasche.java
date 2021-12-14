9abfb3b2507433e62875e93d56c0e34477469f8b
BergKabs <92931496+BergKabs@users.noreply.github.com>
2021-12-14 11:23:52 +0100

    Bombentasche Auftauchen
    auftauchen der Bombentasche mit Wahrscheinlichkeit unter 21%

--- BM/src/application/Objects/bombentasche.java
+++ BM/src/application/Objects/bombentasche.java
@@ -1,18 +1,46 @@
 package application.Objects;
+import java.util.Random;
 
 import application.Client;
 import application.GamePanel;
-
+import application.Ressourcen;
 import javafx.scene.image.Image;
 
 
 public class bombentasche {
 	
-	int bombenanzahltasche=2;
+	int bombenanzahltasche=3;
 	double itemX;
 	double itemY;
 	Image image;
+	static int s;
+	double rangeMin = 0;
+	double rangeMax = 1;
 	
+	//Funktion für die Wahrscheinlichkeit dass eine bombentasche in einer Kiste auftaucht
+	public void itemapear() {
+		for(int x=0;x<16;x++) {
+			for(int y=0;y<16;y++) {
+				Random r = new Random();
+				double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+				if(x%2==0 && x>0 && x<16 && randomValue<0.21) {
+						
+			//Falls wir verschiedene Kistenanordunungen haben passt es sich dem Muster an 
+				//else if(this.kisteX=true && this.kisteY=true) 
+			
+				
+					bombentasche s = new bombentasche();
+					
+					s.bombenanzahltasche=3;
+					
+					s.itemX=x;
+					//s.itemX=this.kisteX;
+					s.itemY=y;	
+					//s.itemY=this.kisteY;
+				}
+		}
+	}	
+}
 	
 	public Image getImage(){
 		
