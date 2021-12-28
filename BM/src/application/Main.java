558b6ba1f729f5cec1b9b2b1effea1538f99363e
MarouanRyad <93085645+MarouanRyad@users.noreply.github.com>
2021-12-28 11:56:32 +0100

    2 players

--- BM/src/application/Main.java
+++ BM/src/application/Main.java
@@ -13,10 +13,20 @@ public class Main extends Application {
 		try {
 		//	player.SetAddress("192.168.1.107");
 		
+<<<<<<< Updated upstream
 			GamePanel game= new GamePanel();
 			game.init();
 //			Parent root = FXMLLoader.load(getClass().getResource("Scenes/LandingPage.fxml"));
 //			Scene scene = new Scene(root);
+=======
+			player.SetAddress("localhost");
+			
+			Ressourcen.readFiles();
+			//GamePanel game= new GamePanel();
+			//game.init();
+		   Parent root = FXMLLoader.load(getClass().getResource("Scenes/LandingPage.fxml"));
+		    Scene scene = new Scene(root);
+>>>>>>> Stashed changes
 			primaryStage.setTitle("Bomberman");
 			primaryStage.setScene(game.getScene());
 			primaryStage.setResizable(false);
---
+++ BM/src/application/PlayerInfos.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.ArrayList;
+
+public class PlayerInfos {
+		
+	String name;
+	String gameUpdates="";
+	public PlayerInfos(String playerName) {
+		name = playerName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String playerName) {
+		name = playerName;
+	}
+	public String GetUpdates() {
+		// TODO Auto-generated method stub	
+		return gameUpdates;
+	}
+	public void SetUpdates(String updates) {
+		// TODO Auto-generated method stub
+		gameUpdates="";
+	}
+	public void AddUpdatesToPlayers(String updates) {
+		// TODO Auto-generated method stub
+		if(gameUpdates.equals("")) {
+			gameUpdates=updates;	
+		}else {
+			gameUpdates=gameUpdates+"-"+updates;
+		}
+		
+	}
+	
+	
+	
+	
+	
+
+	
+}
