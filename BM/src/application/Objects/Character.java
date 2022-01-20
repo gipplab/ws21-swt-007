35140d6c6006da0987ab4567d96cf9c2259a1178
Yazan Sadieh <1942022@uni-wuppertal.de>
2022-01-20 19:34:03 +0100

    jg

--- BM/src/application/Objects/Character.java
+++ BM/src/application/Objects/Character.java
@@ -333,10 +333,12 @@ public void BombanzahlDown(){
 	}
 //Explosion reichweite erhöhen.  
 public void ExplosionUp(){
+	if(this.explosion<5)
 	this.explosion++;
 }
 //Health erhhen.
 public void HealthUp(){
+	if(this.health<3)
 	this.health++;
 }
 public int getHealth(){
@@ -374,13 +376,12 @@ public int indexAnimPlayer() {
 	 }
 	 
 //Hier wird geprft, ob es in diesem Block ein Item gibt.
-		public int isItem(double nextX, double nextY) {
-		// TODO Auto-generated method stub
-		  Entities obje;
-	
+public int isItem(double nextX, double nextY) {
+	// TODO Auto-generated method stub
+		  	Entities obje;
 		     int nextX_1 = (int) (nextX / GamePanel.SQUARE_SIZE);
 		     int nextY_1 = (int) (nextY / GamePanel.SQUARE_SIZE);
-	
+		     
 		     int nextX_2 = (int) ((nextX + GamePanel.SQUARE_SIZE - 1) / GamePanel.SQUARE_SIZE);
 		     int nextY_2 = (int) (nextY / GamePanel.SQUARE_SIZE);
 		    
