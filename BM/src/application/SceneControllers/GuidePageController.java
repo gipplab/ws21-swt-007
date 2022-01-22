2809a611a3379186440b5cd7089dc4661c36dff1
MarouanRyad <93085645+MarouanRyad@users.noreply.github.com>
2022-01-22 23:22:00 +0100

    guide works

--- BM/src/application/SceneControllers/GuidePageController.java
+++ BM/src/application/SceneControllers/GuidePageController.java
@@ -1,9 +1,21 @@
 package application.SceneControllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GuidePageController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 
     @FXML
     private Text DOMPBOMB2;
@@ -46,5 +58,17 @@ public class GuidePageController {
 
     @FXML
     private Text UP2;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void backButtonOnClick(ActionEvent event) throws IOException {
+    	root = FXMLLoader.load(getClass().getResource("/application/Scenes/LandingPage.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+    }
 
 }
