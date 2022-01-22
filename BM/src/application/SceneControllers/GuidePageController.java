3e4949e8418f637330aaa25785c2f77598252769
Janchir <93082473+Janchir@users.noreply.github.com>
2022-01-22 03:11:13 +0100

    UPdate2

---
+++ BM/src/application/SceneControllers/GuidePageController.java
@@ -0,0 +1,50 @@
+package application.SceneControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class GuidePageController {
+
+    @FXML
+    private Text DOMPBOMB2;
+
+    @FXML
+    private Text DOWN;
+
+    @FXML
+    private Text DOWN2;
+
+    @FXML
+    private Text DROPBOMB1;
+
+    @FXML
+    private Text LEFT1;
+
+    @FXML
+    private Text LEFT2;
+
+    @FXML
+    private Text RIGHT1;
+
+    @FXML
+    private Text RIGHT2;
+
+    @FXML
+    private Text T1;
+
+    @FXML
+    private Text T2;
+
+    @FXML
+    private Text TEXT1;
+
+    @FXML
+    private Text TEXT2;
+
+    @FXML
+    private Text UP1;
+
+    @FXML
+    private Text UP2;
+
+}
