2ca7d23e5021e39867b17972aabda4aeb9c34423
Heba-Abdel-Wahab <1836899@uni-wuppertal.de>
2022-01-23 22:21:11 +0100

    update

--- BM/src/application/Client.java
+++ BM/src/application/Client.java
@@ -7,85 +7,92 @@ import java.util.ArrayList;
 public class Client
 {
 	public static String updateString="";
-private static InetAddress host;
-public static ArrayList<String> players=new ArrayList<>();
-public static String playerpseudo;
-public static String roomToJoin;
-private static final int PORT=1234;
-private static DatagramSocket datagramSocket;
-private static DatagramPacket inPacket,outPacket;
-private static byte[] buffer;
-public void SetAddress(String addr){
-	 try {
-		host = InetAddress.getByName(addr);
-		datagramSocket=new DatagramSocket();
-	} catch (UnknownHostException e) {
+	private static InetAddress host; //Javas Darstellung einer IP-Adresse
+	public static ArrayList<String> players=new ArrayList<>();
+	public static String playerpseudo;
+	public static String roomToJoin;
+	private static final int PORT=1234; //Define the destination to send the datagram
+	private static DatagramSocket datagramSocket;
+	private static DatagramPacket inPacket,outPacket;//DatagramPacket-Objekt zum Empfangen und Senden definieren
+
+	private static byte[] buffer;//Byte-Array zum Empfangen von Netzwerkdaten 
+	
+	public void SetAddress(String addr){
+		try {
+			//Inet-Adresse des Servers abrufen
+			host = InetAddress.getByName(addr);
+			//Erstellen eines Datagramm-Sockets
+			datagramSocket=new DatagramSocket();
+		} catch (UnknownHostException e) {
 		// TODO Auto-generated catch block 
 		e.printStackTrace();
-	} catch (SocketException e) {
+		} catch (SocketException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
+		}
 	}
-} 
-public static void main(String[] args)
-  {
-    try
-      {
-         host=InetAddress.getByName("localhost");
-         
-      }
-      catch(UnknownHostException uhEx)
-      {
+	
+	public static void main(String[] args){
+		try{
+		 // Abrufen die IP-Adresse des aktuellen Computers 
+         host=InetAddress.getByName("localhost");  
+		}
+		catch(UnknownHostException uhEx){
          System.out.println("HOST ID not found.. ");
          System.exit(1);
        }
       //serverUpdates();
      }
+	
    public static String accessServer(String message)
-{ 
+   { 
 	   String response="Disconnected";
-try
-  {
+	   try
+	   {
     	//datagramSocket=new DatagramSocket();
          //System.out.println(InetAddress.getLocalHost()+"  enter message :");
          //message=userEntry.nextLine();
          if(!message.equals("***CLOSE***"))
-           {
+         {
         	 //System.out.println(" \n sent msg--<<" +message+">>  "+host);
-         outPacket=new DatagramPacket(message.getBytes(),message.length(),host,PORT);
-         //System.out.println(" \n sent msg--<<" +message+">> 1 "+host);
-         datagramSocket.send(outPacket);
-         datagramSocket.setSoTimeout( 2000 ) ;
-         buffer=new byte[4096];
-         inPacket=new DatagramPacket(buffer,buffer.length);
-         //System.out.println(" \n sent msg--<<" +message+">> 4 "+host);        
-         datagramSocket.receive(inPacket);        
-         //System.out.println(" \n sent msg--<<" +message+">> 5 "+host);
-         response=new String(inPacket.getData(),0,inPacket.getLength());
-          //System.out.println(" \n SERVER-->>" +response);
-          //datagramSocket.close();
+        	 //Senden von Paketen der Länge length an die angegebene Portnummer auf dem angegebenen Host.
+        	 outPacket=new DatagramPacket(message.getBytes(),message.length(),host,PORT);
+        	 //System.out.println(" \n sent msg--<<" +message+">> 1 "+host);
+        	 datagramSocket.send(outPacket);// paket senden
+        	 //ein Aufruf von Receive() für diesen DatagramSocket wird nur für 2 Sekunden blockiert.
+        	 //Wenn das Timeout abläuft, wird eine java.net.SocketTimeoutException ausgelöst,
+        	 datagramSocket.setSoTimeout( 2000 ) ;
+         	 buffer=new byte[4096];
+         	 //Empfangen von Paketen der Länge length.
+         	 inPacket=new DatagramPacket(buffer,buffer.length);
+         	 //System.out.println(" \n sent msg--<<" +message+">> 4 "+host);        
+         	 datagramSocket.receive(inPacket);        
+         	 //System.out.println(" \n sent msg--<<" +message+">> 5 "+host);
+         	 response=new String(inPacket.getData(),0,inPacket.getLength());
+         	 //System.out.println(" \n SERVER-->>" +response);
+         	 //datagramSocket.close();
           return response;          
-      }
-}
-catch(SocketTimeoutException s)
-{
-	System.out.println("NoConnexion");
-	return "NoConnexion";
-}
-catch(IOException ioEx)
-{
- ioEx.printStackTrace();
-}
+         }
+	   }catch(SocketTimeoutException s){
+		// keine Antwort nach 2 Sekunden erhalten.
+		   System.out.println("NoConnexion");
+		   return "NoConnexion";
+	   }
+	   catch(IOException ioEx){//IOException wird ausgelöst, wenn während einer Eingabe-Ausgabe-Operation ein Fehler aufgetreten ist.
+		   ioEx.printStackTrace();
+	   }
 
-finally
-{
-   //System.out.println("\n close connection.... ");
-    //datagramSocket.close();
- }
-return response;
-}
-public static void addPlayer(String player) {
+	   finally
+	   {
+		   //System.out.println("\n close connection.... ");
+		   //datagramSocket.close();
+	   }
+	   return response;
+   }
+   
+   //Hinzufuege einen neuen Spieler zur Liste der Spieler 
+   public static void addPlayer(String player) {
 	// TODO Auto-generated method stub
     players.add(player);
-}
    }
+}
--- BM/src/application/Server.java
+++ BM/src/application/Server.java
@@ -8,14 +8,15 @@ import java.util.ArrayList;
 
 public class Server {
 	private static final int PORT = 1234;
-	private static ArrayList<Room> roomsList = new ArrayList<>();
+	private static ArrayList<Room> roomsList = new ArrayList<>();//Liste zum Speichern von Raumen
 	private static DatagramSocket datagramSocket;
-	private static DatagramPacket inPacket, outPacket;
-	private static byte[] buffer;
+	private static DatagramPacket inPacket, outPacket;// DatagramPacket-Objekt zum Empfangen und Senden definieren
+	private static byte[] buffer;//Byte-Array zum Empfangen von Netzwerkdaten 
 
 	public static void main(String[] args) throws UnknownHostException {
 		System.out.println("opening port \n" + InetAddress.getLocalHost() + "||" + InetAddress.getByName("localhost"));
 		try {
+			//Erstellt und binden den Datagramm-Socket an den angegebenen Port
 			datagramSocket = new DatagramSocket(PORT);
 		} catch (SocketException sockEx) {
 			System.out.println("unable to open ");
@@ -30,75 +31,78 @@ public class Server {
 			InetAddress clientAddress = null;
 			int clientPort;
 			do {
-				buffer = new byte[4096];
+				buffer = new byte[4096];//Byte-Array zum Empfangen von Netzwerkdaten 
 				inPacket = new DatagramPacket(buffer, buffer.length);
-				datagramSocket.receive(inPacket);
-				clientAddress = inPacket.getAddress();
-				clientPort = inPacket.getPort();
-				messageIn = new String(inPacket.getData(), 0, inPacket.getLength());
+				datagramSocket.receive(inPacket);//Empfangen der Paketen von client
+				clientAddress = inPacket.getAddress();//Gibt die IP-Adresse des Clients zurück, von dem das Datagramm empfangen wurde.
+				clientPort = inPacket.getPort();//Gibt die Portnummer des Clients zurück, von dem das Datagram empfangen wurde.
+				messageIn = new String(inPacket.getData(), 0, inPacket.getLength());//konvertiert ein empfangenes DatagramPacket in einen String
 				System.out.println(messageIn);
-				String[] message = messageIn.split("-");
+				String[] message = messageIn.split("-");//  String in mehrere subStrings aufteilen . Trennzeichen "-"
 				
-				if(message[0].equals("Host")) 
+				if(message[0].equals("Host")) // when Client einen Raum erstellen 
 				{
 					PlayerInfos host = new PlayerInfos(message[2]); 
-					Room rm = new Room(message[1],Integer.parseInt(message[3]),Integer.parseInt(message[4]));
-					rm.AddPlayerToRoom(host);
-					roomsList.add(rm);
+					Room rm = new Room(message[1],Integer.parseInt(message[3]),Integer.parseInt(message[4]));// neuer raum erstellen
+					rm.AddPlayerToRoom(host); //Spieler zum Raum hinzufügen
+					roomsList.add(rm);//fuegen den Raum der Raumliste hinzu
 					System.out.print(" : ");
 					System.out.println(messageIn);
 					messageOut = "Created";
 					outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), clientAddress, clientPort);
-					datagramSocket.send(outPacket);
+					datagramSocket.send(outPacket);//Senden an den Client, dass der Raum erstellt wird
 				}else if(message[0].equals("Player")){
-					if(message[1].equals("AllHosts"))
+					if(message[1].equals("AllHosts")) 
 					{
 						messageOut= "";
-						for(Room room : roomsList)
-						{
+						for(Room room : roomsList) 
+						{    //die Raume nennen , wie viele Spieler sich in jedem Raum befinden und die Kapazitaet jedes Raums an den Client senden
 							messageOut =room.getHostName()+"-("+room.GetPlayersNumber()+"/"+room.getHowManyPlayers()+")-"+messageOut ;													
 						}
 						outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), clientAddress, clientPort);
-					    datagramSocket.send(outPacket);
-					}else if(message[1].equals("Join"))
+					    datagramSocket.send(outPacket);// an den Client senden
+					}else if(message[1].equals("Join")) // wenn Spieler einen Raum betreten moechte
 					{
 						boolean found= false;
 							messageOut= "";
 							for(Room room : roomsList)
-							{
-								if(message[2].equals(room.getHostName()))
+							{    // wenn es platz im ausgewaelten raum gibt, fuege den Spieler hinzu
+								if(message[2].equals(room.getHostName()))  
 								{
 									if(room.GetPlayersNumber() < room.getHowManyPlayers())
 									{
 										PlayerInfos player = new PlayerInfos(message[3]); 
 										room.AddPlayerToRoom(player);										
 										found=true;
-										messageOut= "Added";
+										messageOut= "Added"; // send an den Client, dass der Spieler hinzugefuegt ist
 										break;
 									}
 								}
 							}
 							if(!found) 
 							{
-								messageOut= "NoRoomFound";
+								messageOut= "NoRoomFound";// send an den Client, dass es  im ausgewaelten raum gibt
 							}
 							
 							outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), clientAddress, clientPort);
 						    datagramSocket.send(outPacket);
 					}
-				}else if(message[0].equals("Waiting"))
+				}else if(message[0].equals("Waiting"))//wenn der Raumersteller auf andere Spieler wartet
 				{
 						messageOut="";
 						for(Room room : roomsList)
 						{
 							if(message[1].equals(room.getHostName()))
 							{
+								//Wenn die Anzahl der Spieler im Raum nicht vollstaendig ist, 
+								//senden dem Client die Anzahl der Spieler im Raum und die Kapazitaet des Raums
 								if(room.GetPlayersNumber() < room.getHowManyPlayers())
 								{
 									messageOut= room.GetPlayersNumber()+"/"+room.getHowManyPlayers();
 									break;
 								}else if(room.GetPlayersNumber() == room.getHowManyPlayers()) {
-									
+									//Wenn die Anzahl der Spieler im Raum vollstaendig ist,
+									//senden dem Client, dass den Raum vollstaendig ist mit der Namen der Spieler
 									messageOut="Complited-"+room.mapIndex;
 									for(PlayerInfos player : room.players) 
 									{
@@ -127,14 +131,14 @@ public class Server {
 											room.map.remove(i);
 									}
 									
-								}else if(message[5].equals("DEAD")) {
-									room.decreasePlayernumber();
+								}else if(message[5].equals("DEAD")) { // wenn der Spieler getoetet wird
+									room.decreasePlayernumber();//Anzahl der Spieler verringern
 									messageOut="SuccessUpdate";
 								}else {
 									for(PlayerInfos player : room.players) {									
 										if(message[2].equals(player.getName())) {
 											player.setAction(message[4]);
-											if(message[5].equals("BOMB")) {
+											if(message[5].equals("BOMB")) {// wenn Spieler Bombe liegt
 												player.setBomb(message[5]+"-"+message[6]+"-"+message[7]);
 											}else if(message[5].equals("NOBOMB")) {
 												player.setBomb(message[5]);
